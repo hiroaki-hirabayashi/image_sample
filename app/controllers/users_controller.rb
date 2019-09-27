be4bd65b6182938a62d4cf27218a489bee0a36ba
平林 宏淳 <hh@MacBook-Pro.local>
2019-09-28 01:25:02 +0900

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+def create
+  User.create(user_params)
+  # 投稿完了後、すぐに一覧表示画面へ遷移
+  redirect_to :action => :index
+end
+
+
+
+def index
+  @users = User.all
+end
+
+
+private
+
+def user_params
+  params.require(:user).permit(:image)
+end
+
+
+
+end
